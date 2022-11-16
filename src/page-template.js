131f06f195c32c7d04f7828adafc29befe463ba0
Jingsi <gongjingsi@hotmail.com>
2022-11-16 01:52:13 -0500

    capture user input to generate HTML

--- src/page-template.js
+++ src/page-template.js
@@ -1,24 +1,49 @@
-const generateMember = ( member, role) => {
-    var special = '';
-
-    if(role == 'Manager') {
-        special = member.officeNumber;
-    } else if (role == 'Intern') {
-        special = member.school;
-    } else if (role == 'Engineer') {
-        special = member.github;
-    }
+const generateEngineer = (member) => {
 
     return `
     <div class="col-3 card box-shadow">
         <div class="card-header pt-3 pl-4">
             <p>${member.name}</p>
-            <p>${role}</p>
+            <p>Engineer</p>
         </div>
         <div class="card-body p-4">
             <div class="border-blue">${member.id}</div>
             <div class="border-blue">${member.email}</div>
-            <div class="border-blue">${special}</div>
+            <div class="border-blue">${member.github}</div>
+            
+        </div>
+    </div>
+    `
+};
+const generateIntern = (member) => {
+
+    return `
+    <div class="col-3 card box-shadow">
+        <div class="card-header pt-3 pl-4">
+            <p>${member.name}</p>
+            <p>Intern</p>
+        </div>
+        <div class="card-body p-4">
+            <div class="border-blue">${member.id}</div>
+            <div class="border-blue">${member.email}</div>
+            <div class="border-blue">${member.school}</div>
+            
+        </div>
+    </div>
+    `
+};
+const generateManager = ( member) => {
+
+    return `
+    <div class="col-3 card box-shadow">
+        <div class="card-header pt-3 pl-4">
+            <p>${member.name}</p>
+            <p>Manager</p>
+        </div>
+        <div class="card-body p-4">
+            <div class="border-blue">${member.id}</div>
+            <div class="border-blue">${member.email}</div>
+            <div class="border-blue">${member.officeNumber}</div>
             
         </div>
     </div>
@@ -28,10 +53,6 @@ const generateMember = ( member, role) => {
 module.exports = templateData => {
     // destructure page data by section
     const { managers, engineers, interns} = templateData;
-    // console.log(templateData);
-    console.log(managers); console.log(managers[0]);
-    // console.log(interns);
-    // console.log(engineers);
 
     return `
     <!DOCTYPE html>
@@ -52,8 +73,9 @@ module.exports = templateData => {
             <h1 class="page-title text-light py-2 px-3">My Team</h1>
         </header>
         <main class="flex-row my-5 justify-space-around">
-            ${generateMember(managers[0], 'Manager')}
-            
+            ${managers.map(generateManager)}
+            ${interns.map(generateIntern)}
+            ${engineers.map(generateEngineer)}
         </main>
     </body>
     </html>
